4bcd1a8b86b1667321cba4646eff66696ec54994
emilyelhy <1155108739@link.cuhk.edu.hk>
2023-05-12 21:19:04 +0900

    OverviewPage UI on sensitive data updated

--- src/Component/Constant.js
+++ src/Component/Constant.js
@@ -1,21 +1,25 @@
 const DATATYPE = [
-    { name: "media", field: [{ name: "mimeType", type: "cat" }], sensitivity: true },
     { name: "app_usage_event", field: [{ name: "name", type: "cat" }], sensitivity: true },
-    { name: "notification", field: [{ name: "name", type: "cat" }], sensitivity: true },
-    { name: "bluetooth", field: [{ name: "bondState", type: "cat" }], sensitivity: true },
-    { name: "message", field: [{ name: "messageBox", type: "cat" }], sensitivity: true },
+    { name: "call_log", field: [{ name: "type", type: "cat" }], sensitivity: true },
     { name: "location", field: [{ name: "speed", type: "num" }], sensitivity: true },
-    { name: "call_log", field: [{ name: "type", type: "cat" }], sensitivity: false },
-    { name: "wifi", field: [{ name: "numOfAP", type: "count" }], sensitivity: false },
+    { name: "media", field: [{ name: "mimeType", type: "cat" }], sensitivity: true },
+    { name: "message", field: [{ name: "messageBox", type: "cat" }], sensitivity: true },
+    { name: "notification", field: [{ name: "name", type: "cat" }], sensitivity: true },
     { name: "battery", field: [{ name: "level", type: "num" }], sensitivity: false },
+    { name: "bluetooth", field: [{ name: "bondState", type: "cat" }], sensitivity: false },
     { name: "data_traffic", field: [{ name: "rxBytes", type: "num" }, { name: "txBytes", type: "num" }], sensitivity: false },
     { name: "device_event", field: [{ name: "type", type: "cat" }], sensitivity: false },
     { name: "installed_app", field: [{ name: "numOfApps", type: "count" }], sensitivity: false },
-    { name: "physical_activity", field: [{ name: "type", type: "cat" }], sensitivity: false },
-    { name: "physical_activity_transition", field: [{ name: "type", type: "cat" }], sensitivity: false },
+    { name: "wifi", field: [{ name: "numOfAP", type: "count" }], sensitivity: false },
+    // no collected data from ABCLogger is found for below data type
+    // { name: "physical_activity", field: [{ name: "type", type: "cat" }], sensitivity: false },                           // showFewEntries from CategoricalGraph is not compactible for this datatype
+    // { name: "physical_activity_transition", field: [{ name: "type", type: "cat" }], sensitivity: false },
     // { name: "fitness", field: ["stepCount", "calories", "distance"] },
     // { name: "survey", field: ["isEntered"] }
 ];
+
+const SENSITIVE_DATATYPE = DATATYPE.filter(dt => dt.sensitivity);
+const NORMAL_DATATYPE = DATATYPE.filter(dt => !dt.sensitivity);
 
 const COLOURS = ['#7b4173', '#a55194', '#ce6dbd', '#de9ed6', '#8c613c', '#ad7f4f', '#c89b67', '#e5c5a5', '#559867', '#70b182',
     '#90c69b', '#b8e0c6', '#9c6744', '#a56d5d', '#b37d6b', '#c1907c', '#d3a192', '#e0b7a6', '#5d5a6e', '#746f89',
@@ -23,6 +27,6 @@ const COLOURS = ['#7b4173', '#a55194', '#ce6dbd', '#de9ed6', '#8c613c', '#ad7f4f
     '#8b4f4f', '#a15858', '#b76969', '#cc7f7f', '#e09393', '#eca7a7', '#50714f', '#66805d', '#7c926b', '#94a57a',
     '#acc78c', '#c5de9d', '#5a6e5d', '#6f8273', '#8aa38c', '#a2c1a2', '#b8d9b8', '#c9e5c9', '#584f8b', '#6758a1',
     '#7a6db8', '#8c80cc', '#a294e0', '#b8a7ec', '#81504f', '#936063', '#a87a7a', '#bc9191', '#d3a7a7', '#e0b8b8',
-    '#515d5a', '#60736f']
+    '#515d5a', '#60736f'];
 
-export { DATATYPE, COLOURS };
+export { DATATYPE, SENSITIVE_DATATYPE, NORMAL_DATATYPE, COLOURS };
--- src/Component/OverviewPage.js
+++ src/Component/OverviewPage.js
@@ -6,7 +6,7 @@ import BackgroundTimer from 'react-native-background-timer';
 import Geolocation from 'react-native-geolocation-service';
 import RNExitApp from 'react-native-exit-app';
 
-import { DATATYPE } from './Constant';
+import { SENSITIVE_DATATYPE, NORMAL_DATATYPE } from './Constant';
 import { DATATYPE_DESCRIPTION } from './DataTypeDescription';
 import { SERVER_IP_ADDR, SERVER_PORT } from '@env';
 
@@ -104,12 +104,29 @@ export default function OverviewPage({ route }) {
         <SafeAreaView style={{ backgroundColor: "#FEFFBE", flex: 1 }}>
             <Text style={{ fontSize: 18, margin: 15, color: "#000000" }}>Logged in as: {email}</Text>
             <ScrollView>
-                {DATATYPE.map((dt, i) => {
+                <Text style={{ marginHorizontal: 15, marginVertical: 10, color: "#b10000", fontSize: 15, fontWeight: "bold" }}>Sensitive Data</Text>
+                {SENSITIVE_DATATYPE.map((dt, i) => {
                     return (
                         <View key={i} style={{ backgroundColor: (i % 2 ? "#D9D9D9" : "#F3F2F2"), flexDirection: "row", justifyContent: "space-between" }}>
                             <View style={{ flexDirection: "row" }}>
                                 <TouchableOpacity style={{ marginHorizontal: 15, marginVertical: 13 }} onPress={() => navToSetting(dt)}>
-                                    <Text style={{ fontSize: 15, textDecorationLine: "underline", color: "#000000", fontWeight: dt.sensitivity ? "bold" : "normal" }}>{dt.name.charAt(0).toUpperCase() + dt.name.slice(1).replaceAll("_", " ")}</Text>
+                                    <Text style={{ fontSize: 15, textDecorationLine: "underline", color: "#b10000", fontWeight: "bold" }}>{dt.name.charAt(0).toUpperCase() + dt.name.slice(1).replaceAll("_", " ")}</Text>
+                                </TouchableOpacity>
+                                <TouchableOpacity style={{ alignSelf: "center" }} onPress={() => showInfo(dt)}>
+                                    <AntDesign name="questioncircleo" size={15} />
+                                </TouchableOpacity>
+                            </View>
+                            <View style={{ width: 18, height: 18, borderRadius: 9, alignSelf: "center", marginRight: 20, backgroundColor: (status[dt.name] === "on" ? "#128300" : status[dt.name] === "off" ? "#3D3D3D" : "#DC7700") }} />
+                        </View>
+                    )
+                })}
+                <Text style={{ marginHorizontal: 15, marginVertical: 10, color: "#000000", fontSize: 15 }}>Other Data</Text>
+                {NORMAL_DATATYPE.map((dt, i) => {
+                    return (
+                        <View key={i} style={{ backgroundColor: (i % 2 ? "#D9D9D9" : "#F3F2F2"), flexDirection: "row", justifyContent: "space-between" }}>
+                            <View style={{ flexDirection: "row" }}>
+                                <TouchableOpacity style={{ marginHorizontal: 15, marginVertical: 13 }} onPress={() => navToSetting(dt)}>
+                                    <Text style={{ fontSize: 15, textDecorationLine: "underline", color: "#000000", fontWeight: "normal" }}>{dt.name.charAt(0).toUpperCase() + dt.name.slice(1).replaceAll("_", " ")}</Text>
                                 </TouchableOpacity>
                                 <TouchableOpacity style={{ alignSelf: "center" }} onPress={() => showInfo(dt)}>
                                     <AntDesign name="questioncircleo" size={15} />
